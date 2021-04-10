43c58413a6e23a90770249ed06f7c755eb4d5746
Lakshya Toshniwal <lakshyatoshniwal14@gmail.com>
2021-04-10 19:18:15 +0530

    Day-22 Drag N Drop

---
+++ Day-21 Drag N Drop/script.js
@@ -0,0 +1,41 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties) {
+    empty.addEventListener('dragover',dragOver)
+    empty.addEventListener('dragenter',dragEnter)
+    empty.addEventListener('dragleave',dragLeave)
+    empty.addEventListener('drop',dragDrop)
+}
+
+function dragStart() {
+    this.className += ' hold'
+    setTimeout(()=> this.className = 'invisible', 0)
+    
+    console.log('Drag Start') 
+}
+function dragEnd() {
+    this.className = 'fill'
+    console.log('Drag END')
+}
+function dragOver(e) {
+    e.preventDefault()
+    console.log('Drag Over')
+} 
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+    console.log('Drag Enter')
+}
+function dragLeave() {  
+    console.log('Drag Leave')
+    this.className = 'empty'
+}
+function dragDrop() {
+    this.className = 'empty'
+    this.append(fill)
+    console.log('Drag Drop')
+}
