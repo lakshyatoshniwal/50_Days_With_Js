bb9c9e1e85d88c24515486cb18eef4f6b464cb81
Lakshya Toshniwal <lakshyatoshniwal14@gmail.com>
2021-04-17 23:45:13 +0530

    Day-28 Github Profiles

---
+++ Day-28 Github Profiles/script.js
@@ -0,0 +1,85 @@
+const APIURL = 'https://api.github.com/users/'
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username)
+    createUserCard(data)
+    getRepos(username)
+  } catch (err) {
+    if (err.response.status == 404) {
+      createErrorCard('No profile with this username')
+    }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + '/repos?sort=created')
+    addRepostoCard(data)
+  } catch (err) {
+    createErrorCard('Problem fetching repos')
+  }
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+  <div class="card">
+    <h1>${msg}</h1>
+  </div>
+  `
+  main.innerHTML = cardHTML
+}
+
+function addRepostoCard(repos) {
+  const reposEl = document.getElementById('repos')
+  repos.slice(0, 10).forEach((repo) => {
+    const repoLink = document.createElement('a')
+    repoLink.classList.add('repo')
+    repoLink.href = repo.html_url
+    repoLink.target = '_blank' //So that it open in new tab
+    repoLink.innerHTML = repo.name
+
+    reposEl.appendChild(repoLink)
+  })
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+  <div class="card">
+    <div>
+      <img
+        class="avatar"
+        src="${user.avatar_url}"
+        alt="${user.name}"
+      />
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>
+        ${user.bio}
+      </p>
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+      <div id="repos"> 
+        
+      </div>
+    </div>
+  </div>
+  `
+  main.innerHTML = cardHTML
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault() //Usually submit in a file
+  const user = search.value
+  if (user) {
+    getUser(user)
+    search.value = ''
+  }
+})
