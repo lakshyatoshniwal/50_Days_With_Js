8b84a0acb507a9cf11b71f50d495023282c8e325
Lakshya Toshniwal <lakshyatoshniwal14@gmail.com>
2021-04-27 11:00:33 +0530

    Day-31 Password Generator

---
+++ Day-31 Password Generator/script.js
@@ -0,0 +1,84 @@
+const resultElement = document.getElementById('result')
+const lengthElement = document.getElementById('length')
+const uppercaseElement = document.getElementById('uppercase')
+const lowercaseElement = document.getElementById('lowercase')
+const numbersElement = document.getElementById('numbers')
+const symbolsElement = document.getElementById('symbols')
+const generateElement = document.getElementById('generate')
+const clipboardElement = document.getElementById('clipboard')
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
+
+clipboardElement.addEventListener('click', () => {
+  const textarea = document.createElement('textarea')
+  const password = resultElement.innerText
+  if (!password) {
+    return
+  }
+
+  textarea.value = password
+  document.body.appendChild(textarea)
+  textarea.select()
+  document.execCommand('copy')
+  textarea.remove()
+  alert('Password copied to clipboard')
+})
+
+generateElement.addEventListener('click', () => {
+  const length = +lengthElement.value
+  const hasLower = lowercaseElement.checked
+  const hasUpper = uppercaseElement.checked
+  const hasNumber = numbersElement.checked
+  const hasSymbol = symbolsElement.checked
+
+  resultElement.innerText = generatePasswoord(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  )
+})
+
+function generatePasswoord(lower, upper, number, symbol, length) {
+  let generatedPassword = ''
+  const typesCount = lower + upper + number + symbol
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  )
+
+  if (typesCount === 0) {
+    return ''
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0]
+      generatedPassword += randomFunc[funcName]()
+    })
+  }
+
+  return generatedPassword.slice(0, length)
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
